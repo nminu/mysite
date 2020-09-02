565f59e5e4fc62f319f8388654d54dae43ea697e
nminu <60621959+nminu@users.noreply.github.com>
2020-09-02 16:46:52 -0500

    sizing edits

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <img src={selfpic} alt='yeet' width='100%'/>
         </div>
       <header className="App-header">
-        <img src={pic1} alt='pic' width='100%' height ='200px'/>
+        <img src={pic1} alt='pic' width='100%' height ='350px'/>
       </header>
       <video autoplay='autoplay' loop ='loop' muted className='backvideo'>
           <source src = {backvid} type = 'video/mp4' />
