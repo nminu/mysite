4864b3e2161f06e0c0b59af06caf63dd974371d4
nminu <60621959+nminu@users.noreply.github.com>
2020-09-02 15:18:46 -0500

    background vid and ignore mp4 files
    edited html and css for background video to show up and included mp4 files in gitignore so I can have them in the resource folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import pic1 from './resources/pic1.jpg';
 import selfpic from './resources/selfpic.jpg';
+import backvid from './resources/backvid.mp4';
 import './App.css';
 
 function App() {
   return (
     <body className="App">
-        <div className='imgbox'>
-            <img src={selfpic} alt='yeet' width='100%'/>
-         </div>
+      <div className='imgbox'>
+          <img src={selfpic} alt='yeet' width='100%'/>
+        </div>
       <header className="App-header">
         <img src={pic1} alt='pic' width='100%' height ='200px'/>
       </header>
+      <video autoplay='autoplay' loop ='loop' muted className='backvideo'>
+          <source src = {backvid} type = 'video/mp4' />
+      </video>
       <main>
         <section className = 'mainbox'>
           <p>
