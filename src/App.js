09208ecc9fa2552ebc18f47a384ed8ee6be43e7b
nminu <60621959+nminu@users.noreply.github.com>
2020-09-02 14:50:41 -0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import pic1 from './resources/pic1.jpg';
+import selfpic from './resources/selfpic.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <body className="App">
+        <div className='imgbox'>
+            <img src={selfpic} alt='yeet' width='100%'/>
+         </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={pic1} alt='pic' width='100%' height ='200px'/>
       </header>
-    </div>
+      <main>
+        <section className = 'mainbox'>
+          <p>
+            yeet
+          </p>
+        </section>
+      </main>
+    </body>
   );
 }
 
