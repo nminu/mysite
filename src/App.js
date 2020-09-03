c830812a225302aaefc6b4cca5d213a7db909f2a
nminu <60621959+nminu@users.noreply.github.com>
2020-09-03 14:55:51 -0500

    photoshop changes and added pics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import pic1 from './resources/pic1.jpg';
-import selfpic from './resources/selfpic.jpg';
+import backdrop2 from './resources/backdrop2.png';
+import sun2 from './resources/sun2.png';
+import selfpic from './resources/selfpic.png';
+import test from './resources/test.png';
 import backvid from './resources/backvid.mp4';
 import './App.css';
 
@@ -11,13 +13,15 @@ function App() {
           <img src={selfpic} alt='yeet' width='100%'/>
         </div>
       <header className="App-header">
-        <img src={pic1} alt='pic' width='100%' height ='350px'/>
+        <img src={backdrop2} alt='pic' width='100%' height ='350px'/>
+        <img src={sun2} alt='pic' className='sun'/>
       </header>
       <video autoplay='autoplay' loop ='loop' muted className='backvideo'>
           <source src = {backvid} type = 'video/mp4' />
       </video>
       <main>
         <section className = 'mainbox'>
+          <img src={test} alt='back' width='100%'/>
           <p>
             yeet
           </p>
