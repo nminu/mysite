fa04a00e810402ff3a330531b789b00df6097118
nminu <60621959+nminu@users.noreply.github.com>
2020-09-09 12:31:26 -0500

    text

--- src/components/Slider/aboutme.js
+++ src/components/Slider/aboutme.js
@@ -9,12 +9,11 @@ class Aboutme extends React.Component {
         return (
             <div className='maindiv'>
                 <section class="box">
-
                 <img src={aboutme} alt="img01" width='400px' className='amimg'/>
                     <Fade bottom>
                         <p className='text'>I'm<span> Nick Minutillo</span>, a web developer based out of <span>Austin, TX</span>.
                             After living in Florida for most of my life I attended Penn State University and earned my engineering degree.
-                            I enjoy coding websites and games, creating graphics in Photoshop, playing video games, and watching hockey (<span>LGR</span>).
+                            I enjoy coding, creating graphics in Photoshop, playing video games, and watching hockey (<span>LGR</span>).
                             This site was made mostly for fun but also to showcase a few of my skills.
                             It's largely a work in progress and I have a few exciting plans for it.
                         </p>
