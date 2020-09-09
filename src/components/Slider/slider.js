a23c1de42b766c2fe947793831d93d748cea6f67
nminu <60621959+nminu@users.noreply.github.com>
2020-09-09 15:10:21 -0500

    buttons change color now

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -7,19 +7,28 @@ function Slider() {
     const [y, setY] = useState(0)
     const go1=() => {
         setY(0);
+        document.getElementById('go1').className='active';
+        document.getElementById('go2').className='inactive';
+        document.getElementById('go3').className='inactive';
     }
     const go2=() => {
         setY(-100);
+        document.getElementById('go1').className='inactive';
+        document.getElementById('go2').className='active';
+        document.getElementById('go3').className='inactive';
     }
     const go3=() => {
         setY(-200);
+        document.getElementById('go1').className='inactive';
+        document.getElementById('go2').className='inactive';
+        document.getElementById('go3').className='active';
     }
     return (
         <div className='slider'>
 
-            <button id="go1" onClick={go1}></button>
-            <button id="go2" onClick={go2}></button>
-            <button id="go3" onClick={go3}></button>
+            <button id="go1" onClick={go1} className='active'></button>
+            <button id="go2" onClick={go2} className='inactive'></button>
+            <button id="go3" onClick={go3} className='inactive'></button>
 
             {sliderArr.map((item,index) => {
                 return (
