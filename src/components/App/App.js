5988f7bde559e1e11767299ba1a0b56f10927e7c
nminu <60621959+nminu@users.noreply.github.com>
2020-09-13 17:16:48 -0500

    general layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import backdrop2 from '../../resources/backdrop2.png';
 import sun2 from '../../resources/sun2.png';
 import FaceBox from '../Facebox/facebox';
-import trees from '../../resources/treecar.png'
+import trees from '../../resources/treebottom.png'
+import pink from '../../resources/linetest2.png'
 import Slider from '../Slider/slider';
 import './App.css';
 import Buttons from '../Buttons/buttons';
@@ -20,23 +21,23 @@ function App() {
       <main>
 
         <section className = 'introbox'>
-          <h1>WELCOME</h1>
-          <p>
-            I'm Nick Minutillo <br />
-            Click below to read about me <br />
-           Also check out some of my latest projects <br />
-            Get in touch by email, or by using the form below
+          <h1>Welcome</h1>
+          <p className='introtext'>
+            <span className='gradient'>I'm Nick Minutillo</span> <br />
+            <span className='gradient'>Click below to read about me</span> <br />
+            <span className='gradient'>Also check out some of my latest projects</span> <br />
+            <span className='gradient'>Get in touch by email, or by using the form below</span>
           </p>
         </section>
 
         <section className ='otherboxes'>
-
+          <Buttons />
         </section>
 
       </main>
 
       <footer>
-
+      <img src={trees} className='trees'></img>
       </footer>
 
     </body>
