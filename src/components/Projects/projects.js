87f4441d7f45d113a4a87305f5f73832994499e9
nminu <60621959+nminu@users.noreply.github.com>
2020-09-28 12:57:10 -0500

    some progress with projects cards

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -5,15 +5,53 @@ class Projects extends React.Component {
 
     render () {
         return (
-        <div className='projectsmainbox'>
-            <div className='aproject'>
-                <p>ayy</p>
+        <div className='pwrapper'>
+            <div className='pcard'>
+                <div className='poverlay'>
+                    <div className='pinfo'>
+                        <h1>
+                            Synth
+                        </h1>
+                        <p>
+                            This website is yeet
+                        </p>
+                        <button>
+
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className='aproject'>
-                <p>ayy2</p>
+
+            <div className='pcard'>
+                <div className='poverlay'>
+                    <div className='pinfo'>
+                        <h1>
+                            Tooltensils
+                        </h1>
+                        <p>
+                            This website is yeet
+                        </p>
+                        <button>
+
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className='aproject'>
-                <p>ayy3</p>
+
+            <div className='pcard'>
+                <div className='poverlay'>
+                    <div className='pinfo'>
+                        <h1>
+                            Synth
+                        </h1>
+                        <p>
+                            This website is yeet
+                        </p>
+                        <button>
+
+                        </button>
+                    </div>
+                </div>
             </div>
 
         </div>
