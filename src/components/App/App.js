c14a1292f4f0a991566ac234cdf2df8f777f85be
nminu <60621959+nminu@users.noreply.github.com>
2020-09-30 15:28:49 -0500

    testing about section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FaceBox from '../Facebox/facebox';
 import Para from '../Para/para.js'
+import aboutme from '../../resources/aboutme.png'
 import './App.css';
 
 
@@ -18,6 +19,28 @@ function App() {
         
 
       <Para />
+
+      <div className='aboutMe'>
+        <div className ='information'>
+          <div className='infopic'>
+            <img src={aboutme}></img>
+          </div>
+          <div className='infocontent'>
+            <div>
+              <div className='line'></div>
+              <div className='line line2'></div>
+            </div>
+            <div>
+              <p className='infotext'> I yeet and yaat and definitely have yote. Test test test</p>
+            </div>
+            <button className='infobutton'>Read More</button>
+          </div>
+
+        </div>
+        <div className='pic'>
+
+        </div>
+      </div>
 
 
       </main>
